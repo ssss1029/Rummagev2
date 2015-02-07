2cef0963c223496348811f8802bcc1f9194adb99
ssss1029 <sauravkadavath@gmail.com>
2015-02-07 12:19:16 -0800

    Anurag FIx this shit on bootstarp or imma slaughter you

--- js/myjs.js
+++ js/myjs.js
@@ -1,9 +1,47 @@
 
 
 $(document).ready(function() {	
+	
+	$("#closeGoogle").hide(); 
+	$("#closeGithub").hide(); 
+	$("#closeDropbox").hide(); 
+	$("#closeBox").hide(); 
+
+	var googleAuthTokenVariable;
+	
 	document.querySelector("#googlebutton").addEventListener("click", function() {
 		chrome.identity.getAuthToken({"interactive": true}, function(token) {
-			alert(token); 
+			googleAuthTokenVariable = token;
+			alert("Google Authentication Token: " + token);
+			chrome.cookies.set({url:'http://www.google.com/', name:'token', value:token}, function(cookie) {
+				alert("Cookie made XDD"); 
+				$("#googlebutton").hide(); 
+				$("#closeGoogle").show(); 
+			})
 		});
 	});
+
+	$("#closeGoogle").click(function() {
+		chrome.cookies.remove({url:'http://www.google.com/', name:'token'}); 
+		chrome.identity.removeCachedAuthToken({token:googleAuthTokenVariable}, function() {
+			//hi
+		}); 
+		$("#googlebutton").show(); 
+	});
+
+	var boxAuthTokenVariable; 
+
+	$("#boxbutton").click(function() {
+		chrome.identity.launchWebAuthFlow( 
+			{
+				'url': 'https://app.box.com/api/oauth2/authorize?response_type=code&client_id=knj0e2chk6sgpwp41thyx7kud2cgct8z&state=security_token%3DKnhMJatFipTAnM0nHlZA', 
+				'interactive': true
+			}, 
+			function(redirect_url) { 
+				alert(redirect_url); 
+			}
+		);
+	});
+
+
 });
